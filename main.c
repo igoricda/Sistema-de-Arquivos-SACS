663b89c5014db6116307367a56f33997d69a26e4
igor <igoricda@gmail.com>
2026-01-15 16:58:03 -0300

    formatacao do sacs pronta, criacao de arquivos em progresso

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <limits.h>
 
 #define ENTRY_SIZE 32
+#define directory_entry 0x0001
+#define file_entry 0x0002
+#define deleted_entry 0x0003
 
 struct __attribute__((__packed__)) superblock {
     unsigned sysid;
@@ -17,7 +21,7 @@ struct __attribute__((__packed__)) superblock {
     unsigned root_size;
     unsigned root_start;
     unsigned data_start;
-    char reserved[32];
+    char reserved[24];
 };
 
 struct __attribute__((__packed__)) dir_entry{
@@ -26,21 +30,9 @@ struct __attribute__((__packed__)) dir_entry{
     unsigned short file_type;
     unsigned start_block;
     unsigned size;
-    unsigned length;
+    unsigned length_in_blocks;
 };
 
-struct __attribute__((__packed__)) superblock_disk {
-    unsigned int sysid;           // Offset 0
-    unsigned short sector_size;   // Offset 4
-    unsigned int total_blocks;    // Offset 6
-    unsigned short block_size;    // Offset 10
-    unsigned int bitmap_start;    // Offset 12
-    unsigned int bitmap_size;     // Offset 16
-    unsigned int root_start;      // Offset 20
-    unsigned int root_size;       // Offset 24
-    unsigned int data_start;      // Offset 28
-    char reserved[32];            // Offset 32 (Preencher com zeros)
-};
 
 void set_bit(unsigned char *bitmap_buffer, int block_index) {
     int byte_offset = block_index / 8; // Acha a posição do byte no vetor
@@ -51,34 +43,52 @@ void set_bit(unsigned char *bitmap_buffer, int block_index) {
     bitmap_buffer[byte_offset] |= (1 << bit_offset);
 }
 
-void main(){
+// Função para ler o estado do bit (0 ou 1)
+int get_bit(unsigned char *bitmap, int index) {
+    int byte_offset = index / 8;
+    int bit_offset  = index % 8;
+    return (bitmap[byte_offset] >> bit_offset) & 1;
+}
+
+
+void build_superblock (struct superblock *sup, unsigned int sysid, unsigned sector_count, unsigned short sector_size, unsigned short block_size, unsigned int root_size){
+    sup->sysid = sysid;
+    sup->sector_count = sector_count;
+    sup->sector_size = sector_size;
+    sup->block_size = block_size;
+    sup->sectors_per_block = (1 << sup->block_size) ;
+    sup->total_blocks = (sup->sector_count + (sup->sectors_per_block-1)) / sup->sectors_per_block;  
+    sup->bitmap_start = 1;
+    sup->bitmap_size = (sup->total_blocks/(1<<(sup->sector_size)<<sup->block_size))+1;
+    sup->root_start = sup->bitmap_start + sup->bitmap_size;
+    sup->root_size = root_size;
+    sup->data_start = sup->root_start + sup->root_size;         
+}
+
+void print_sup(struct superblock *sup){
+    
+    printf("Sysid = %u\n", sup->sysid);
+    printf("Sector_size = %hu = %u \n", sup->sector_size, 1 << (sup->sector_size));
+    printf("Sector_Count = %u\n", sup->sector_count);
+    printf("Sectors per block = %u\n", sup->sectors_per_block);
+    printf("Total Blocks = %u\n", sup->total_blocks);
+    printf("Block size = %hu = %u\n", sup->block_size, (1 << (sup->sector_size)) << sup->block_size);
+    printf("Bitmap Start = %u\n", sup->bitmap_start);
+    printf("Bitmap Size = %u\n", sup->bitmap_size);
+    printf("Root Start = %u\n", sup->root_start);
+    printf("Root Size = %u\n", sup->root_size);
+    printf("Data Start = %u\n", sup->data_start);
+}
+
+
+void format_sacs(unsigned int sysid, unsigned sector_count, unsigned short sector_size, unsigned short block_size, unsigned int root_size){
     struct superblock sup;
     memset(&sup, 0, sizeof(struct superblock));
+    build_superblock(&sup, sysid, sector_count, sector_size, block_size, root_size);
+    print_sup(&sup);
+    printf("Size of SuperBlock = %lu\nSize of DirEntry = %lu\n",
+            sizeof(struct superblock), sizeof(struct dir_entry));
 
-    sup.sysid = 1;
-    //scanf("%u", &sup.sector_count);
-    sup.sector_count = 80;
-    sup.sector_size = 9;
-    sup.block_size = 2;
-    sup.sectors_per_block = (1 << sup.block_size) ;
-    sup.total_blocks = (sup.sector_count + (sup.sectors_per_block-1)) / sup.sectors_per_block;  
-    sup.bitmap_start = 1;
-    sup.bitmap_size = (sup.total_blocks/(1<<(sup.sector_size)<<sup.block_size))+1;
-    sup.root_start = sup.bitmap_start + sup.bitmap_size;
-    sup.root_size = 4;
-    sup.data_start = sup.root_start + sup.root_size;
-
-    printf("Sysid = %u\n", sup.sysid);
-    printf("Sector_size = %hu =  %u \n", sup.sector_size, 1 << (sup.sector_size));
-    printf("Sector_Count = %u\n", sup.sector_count);
-    printf("Sectors per block = %u\n", sup.sectors_per_block);
-    printf("Total Blocks = %u\n", sup.total_blocks);
-    printf("Block size = %hu = %u\n", sup.block_size, (1 << (sup.sector_size)) << sup.block_size);
-    printf("Bitmap Start = %u\n", sup.bitmap_start);
-    printf("Bitmap Size = %u\n", sup.bitmap_size);
-    printf("Root Start = %u\n", sup.root_start);
-    printf("Root Size = %u\n", sup.root_size);
-    printf("Data Start = %u\n", sup.data_start);
 
     FILE *fp = fopen("sacs.img", "wb");
     if (!fp) {
@@ -88,12 +98,9 @@ void main(){
 
     printf("Arquivo criado\n");
     unsigned int real_block_size = (1 << (sup.sector_size)) << sup.block_size;
-
     unsigned char *buffer = calloc(1, real_block_size);
     memcpy(buffer, &sup, sizeof(struct superblock));
     fwrite(buffer, real_block_size , 1, fp);
-
-    printf("linha 94\n");
     unsigned int metadata_blocks = 1 + sup.bitmap_size + sup.root_size;
 
     // Limpa o buffer para usar no bitmap
@@ -116,15 +123,133 @@ void main(){
     unsigned int data_blocks = sup.total_blocks - sup.data_start;
     printf("data blocks = %d\n", data_blocks);
     memset(buffer, 0, real_block_size);
-    /** */
+    
     for (unsigned int i = 0; i < data_blocks; i++) {
         fwrite(buffer, real_block_size, 1, fp);
         printf("data write %d\n", data_blocks -i);
-    }//*/
+    }
 
     fclose(fp);
     free(buffer);
     printf("Arquivo sacs.img criado com sucesso!\n");
+}
+
+struct superblock read_super_block(FILE *fp){
+    struct superblock  superblock;
+    fseek(fp, 0, SEEK_SET);
+    fread(&superblock, sizeof(superblock),1, fp);
+    return superblock;
+}
+
+
+
+
+unsigned contiguous_alloc(FILE *fp, unsigned file_size, unsigned block_size, unsigned bitmap_start,
+                      unsigned total_blocks){
+    unsigned blocks_needed = (file_size + block_size - 1) / block_size;
+    if(blocks_needed == 0) return -1;
+    
+
+    unsigned bitmap_byte_count = (total_blocks + 7) / 8;
+    unsigned char *bitmap = (unsigned char *)malloc(bitmap_byte_count);
+
+    if (!bitmap) return -1;
+    
+    long bitmap_offset = (long)bitmap_start * block_size;
+    fseek(fp, bitmap_offset, SEEK_SET);
+    fread(bitmap, 1, bitmap_byte_count, fp);
+    
+    int best_start = -1;
+    unsigned int best_len = UINT_MAX;
+
+    int current_start = -1;
+    unsigned int current_len = 0;
+    
+    for (unsigned int i = 0; i < total_blocks; i++) {
+        // Verifica o bit i (0 = Livre, 1 = Ocupado)
+        if (get_bit(bitmap, i) == 0) {
+            // É um bloco livre
+            if (current_start == -1) current_start = i;
+            current_len++;
+        } else {
+            // Encontrou um bloco ocupado, fim da sequência livre atual
+            if (current_start != -1) {
+                // A sequência que acabou serve?
+                if (current_len >= blocks_needed) {
+                    // É melhor (menor sobra) que a anterior?
+                    if (current_len < best_len) {
+                        best_len = current_len;
+                        best_start = current_start;
+                        
+                        // Otimização: Se o tamanho for exato, não vai achar melhor
+                        if (best_len == blocks_needed) break;
+                    }
+                }
+                // Reseta para procurar a próxima
+                current_start = -1;
+                current_len = 0;
+            }
+        }
+    }
+
+    if (current_start != -1 && current_len >= blocks_needed) {
+        if (current_len < best_len) {
+            best_start = current_start;
+        }
+    }
+
+    if (best_start != -1) {
+        for (unsigned int i = 0; i < blocks_needed; i++) {
+            set_bit(bitmap, best_start + i);
+        }
+
+        // Volta o ponteiro para o início do bitmap e escreve as alterações
+        fseek(fp, bitmap_offset, SEEK_SET);
+        fwrite(bitmap, 1, bitmap_byte_count, fp);
+    }
+
+    free(bitmap);
+    return best_start;
+}
+
+void create_dir_entry(struct dir_entry *entry, char file_name[17], unsigned short file_type, 
+                      unsigned size, unsigned start_block, unsigned block_size){
+    
+    entry->status = 1;
+    strcpy(entry->file_name, file_name);
+    entry->file_type = file_type;
+    entry->size = size;
+    entry->start_block = start_block;
+    entry->length_in_blocks = (size + block_size - 1) / block_size;
+}
+
+void create_dir(FILE *fp, struct superblock *sup, char file_name[17], unsigned size){
+    struct dir_entry entry;
+    memset(&entry, 0, sizeof(struct dir_entry));
+    unsigned file_start = contiguous_alloc(fp, size, 1 >> sup->sector_size >> sup->block_size, 
+                                           sup->bitmap_start, sup->total_blocks);
+    
+    //Setar fp no inicio de onde tem que escrever
+    //criar entradas ./ e ../ nos blocos de dados indicados no bitmap
+    
+
+
+
+
+}
+
+void create_file(){}
+
+void main(){
+    format_sacs(1, 80, 9, 2, 4);
+
+    FILE *fp;
+
+    fp= fopen("./sacs.img", "rb");
+    struct superblock  superblock = read_super_block(fp);
+    print_sup(&superblock);
+    //Criar diretorio raiz com ./ e ../ levando a si mesmo
+
 
 
     
